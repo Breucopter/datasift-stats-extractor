6d0a9a1ae522bc741c695afa531c12c2f686ea0b
Ben Hagan <benhagan@gmail.com>
2013-12-05 11:14:00 +0000

    Config to enable/disable extrator groups.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+var start = process.hrtime();
+var elapsed_time = function(note){
+    var precision = 3; // 3 decimal places
+    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+    start = process.hrtime(); // reset the timer
+}
+
 var fs = require('fs')
 	, byline = require('byline')
 	, extract = require('./lib/extract')
@@ -7,8 +15,9 @@ var fs = require('fs')
 	, counter = 0
 	;
 
-var stream = fs.createReadStream(file);
-		stream = byline.createStream(stream);
+var 	stream = fs.createReadStream(file)
+		,	stream = byline.createStream(stream)
+		;
 
 // Data Objects
 global.timestamps 	= {};
@@ -41,5 +50,6 @@ stream.on('data', function(line) {
 stream.on('end', function() {
   console.log("\nBuilding result sets...");
   dataexport.dumpFiles();
+	elapsed_time("Complete.");
 });
 
